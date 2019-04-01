a89be9534462e1bf90613839c757ff130eee0907
Ziyan Huang <huang.ziya@husky.neu.edu>
2019-04-01 11:38:21 -0700

    A5 final version 0.4 prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  /*baseUrl: 'http://localhost:3200'*/
-  baseUrl: 'https://webdev-ziyan.herokuapp.com'
+  baseUrl: 'http://localhost:3200'
 };
 
 /*
